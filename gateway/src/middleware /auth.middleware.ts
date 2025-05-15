f7449c135aacc9ca433602e3bc2706902d2155bf
jsj <tmdwn3318@gmail.com>
2025-05-15 16:23:14 +0900

    에러 메세지 수정

--- gateway/src/middleware /auth.middleware.ts
+++ gateway/src/middleware /auth.middleware.ts
@@ -1,4 +1,4 @@
-import { Injectable, NestMiddleware, UnauthorizedException, ForbiddenException } from '@nestjs/common';
+import { Injectable, NestMiddleware, UnauthorizedException, ForbiddenException, NotFoundException } from '@nestjs/common';
 import { Request, Response, NextFunction } from 'express';
 import { ACCESS_RULES } from '../access-map';
 import * as jwt from 'jsonwebtoken';
@@ -14,14 +14,14 @@ export class AuthMiddleware implements NestMiddleware {
         // console.log('originalUrl:', req.originalUrl);
         // console.log('matched rule:', matched);
 
-        if (!matched) throw new ForbiddenException('잘못된 요청');
+        if (!matched) throw new NotFoundException('api not found in gateway');
 
         if (matched.roles.length === 0) {
             return next();
         }
 
         const token = req.headers.authorization?.split(' ')[1];
-        if (!token) throw new UnauthorizedException('토큰 없음!');
+        if (!token) throw new UnauthorizedException('no token!');
 
         try {
             const decoded = jwt.verify(token, process.env.JWT_SECRET);
@@ -31,12 +31,12 @@ export class AuthMiddleware implements NestMiddleware {
             if(decoded.role === 'ADMIN') return next();
 
             if (!matched.roles.includes(decoded.role)) {
-                throw new ForbiddenException('권한 없음!');
+                throw new ForbiddenException('forbidden');
             }
 
             next();
         } catch (e) {
-            throw new UnauthorizedException('잘못된 토큰!');
+            throw new UnauthorizedException('wrong token!');
         }
     }
 
