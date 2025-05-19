9b6b18e2a3033e18f1eb899912fdcaeeb2343b16
jsj <tmdwn3318@gmail.com>
2025-05-19 15:29:13 +0900

    유저 이벤트 진행도 스키마에 스웨거 설정

--- event/src/event/user-event-progress/dto/increase-condition-progress.dto.ts
+++ event/src/event/user-event-progress/dto/increase-condition-progress.dto.ts
@@ -1,10 +1,14 @@
 import { IsString, IsNumber, Min } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class IncreaseConditionProgressDto {
+
     @IsString()
+    @ApiProperty({ description: '조건 타입', example: 'INVITE_COUNT'})
     type: string;
 
     @IsNumber()
     @Min(1)
+    @ApiProperty({ description: '증가할 조건 수치', example: 1, minimum: 1 })
     amount: number;
 }
--- event/src/event/user-event-progress/user-event-progress.controller.ts
+++ event/src/event/user-event-progress/user-event-progress.controller.ts
@@ -1,12 +1,21 @@
 import { Controller, Patch, Param, Body, Get, NotFoundException } from '@nestjs/common';
+import {ApiOperation, ApiResponse, ApiParam, ApiTags} from '@nestjs/swagger';
 import { IncreaseConditionProgressDto } from './dto/increase-condition-progress.dto';
 import { UserEventProgressService } from './user-event-progress.service';
 
+@ApiTags('User Event Progress')
 @Controller('api/event/:eventId/user/:userId/progress')
 export class UserEventProgressController {
   constructor(private readonly userEventProgressService: UserEventProgressService) {}
 
+
   @Patch()
+  @ApiOperation({ summary: '조건 진행도 증가' })
+  @ApiResponse({ status: 200, description: '진행도 증가 성공' })
+  @ApiResponse({ status: 400, description: '이벤트 진행 중 아니거나, 증가하려는 조건 타입 없음' })
+  @ApiResponse({ status: 404, description: '이벤트 없음' })
+  @ApiParam({ name: 'eventId', description: '이벤트 ID' })
+  @ApiParam({ name: 'userId', description: '유저 ID' })
   async increaseConditionProgress(
     @Param('eventId') eventId: string,
     @Param('userId') userId: string,
@@ -21,9 +30,14 @@ export class UserEventProgressController {
   }
 
   @Get()
+  @ApiOperation({ summary: '특정 유저의 이벤트 조건 진행도 조회' })
+  @ApiResponse({ status: 200, description: '진행도 조회 성공' })
+  @ApiResponse({ status: 404, description: '진행도 없음' })
+  @ApiParam({ name: 'eventId', description: '이벤트 ID' })
+  @ApiParam({ name: 'userId', description: '유저 ID' })
   async getUserEventProgress(
-    @Param('userId') userId: string,
-    @Param('eventId') eventId: string
+    @Param('eventId') eventId: string,
+    @Param('userId') userId: string
   ) {
     return this.userEventProgressService.getUserEventProgress(userId, eventId);
   }
